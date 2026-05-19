82bf7f38eb228d5643c31c7f284983bf661aba76
Hgo92 <hugo_monier@yahoo.fr>
2026-05-19 20:03:22 +0200

    erreur baseURL

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  baseURL: "/api/auth",
+  baseURL: "https://learn-with-me-nest.vercel.app",
   fetchOptions: {
     credentials: "include",
   },
